c3498a403fdcad9cd0dd83ec2290690c093924fb
Anton <anton.maximov@gmail.com>
2013-05-21 18:09:05 +0400

    User info and update user data

--- wuser/views.py
+++ wuser/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8
 from django.http import HttpResponse
-import json, datetime
+import json
+import datetime
+from django.core import serializers
 from models import WUser
 from django.contrib.auth.models import User
 from django.contrib import auth
@@ -63,9 +65,58 @@ def login(username, password):
         return json.dumps(result)
 
 
-# Метод основной обработки запросов
-# Тут мы проверяем что мы должны сделать и куда перенаправить обработку
-def main(request):
+def user_data(id, token):
+    try:
+        wu = WUser.objects.get(id=id)
+        if wu.token == token:
+            result = dict()
+            result['code'] = 200
+            result['data'] = serializers.serialize('json', [wu])
+            return json.dumps(result)
+        else:
+            result = dict()
+            result['code'] = 401
+            result['message'] = 'Incorrect token'
+            return json.dumps(result)
+    except WUser.DoesNotExist:
+        result = dict()
+        result['code'] = 401
+        result['message'] = 'User not founded'
+        return json.dumps(result)
+
+
+def update(id, token, data):
+    try:
+        wu = WUser.objects.get(id=id)
+        if wu.token == token:
+            j_data = json.loads(data)
+
+            if 'email' in j_data:
+                wu.email = j_data['email']
+            if 'password' in j_data:
+                wu.set_password(j_data['password'])
+            if 'avatar' in j_data:
+                wu.avatar = j_data['avatar']
+            wu.save()
+
+            result = dict()
+            result['code'] = 200
+            return json.dumps(result)
+        else:
+            result = dict()
+            result['code'] = 401
+            result['message'] = 'Incorrect token'
+            return json.dumps(result)
+    except WUser.DoesNotExist:
+        result = dict()
+        result['code'] = 401
+        result['message'] = 'User not founded'
+        return json.dumps(result)
+
+
+# Main request method
+# Check action type and call method
+def main(request, id=0, token=""):
     # Если это метод POST - это однозначно создание новой записи
     if 'POST' == request.method:
         return HttpResponse(registration(request.body))
@@ -73,10 +124,18 @@ def main(request):
     # Если это метод GET
     elif 'GET' == request.method:
         # Если нам передан id и экшн login
-        username = request.GET["username"]
-        password = request.GET["password"]
+        username = ""
+        password = ""
+        if "username" in request.GET:
+            username = request.GET["username"]
+        if "password" in request.GET:
+            password = request.GET["password"]
         if username != "" and password != "":
             return HttpResponse(login(username, password))
+        elif id > 0 and token != "":
+            return HttpResponse(user_data(id, token))
+    elif 'PUT' == request.method:
+        return HttpResponse(update(id, token, request.body))
 
     # Либо возвращает что метод запроса не верный
     return HttpResponse('Incorrect method')
