741ef89f4751b93c638080a6314e53e54638fe12
Anton <anton.maximov@gmail.com>
2013-06-03 20:56:57 +0400

    Fix rel

--- wobject/views.py
+++ wobject/views.py
@@ -145,7 +145,7 @@ def select(name, data=''):
                     tmpF = 'id' + rel
                     if f.upper() == tmpF.upper():
                         idValue = row[f]
-                        rel_param = 'id=' + str(idValue)
+                        rel_param = '{"where": "id=' + str(idValue) + '"}'
                         row[rel] = get_raw_data(rel, rel_param)[0]
                         row[tmpF] = None
                         break
